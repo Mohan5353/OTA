e0374b2a21a2655b0f5e9d000458d18ab1ff657b
Mohan5353 <120332828+Mohan5353@users.noreply.github.com>
2023-11-12 19:06:34 +0530

    Update main.py

--- firmwall/main.py
+++ firmwall/main.py
@@ -1,5 +1,5 @@
 # No implemented Code bro
-from uos import remove,listdir()
+from uos import remove,listdir
 files=listdir()
 for file in files:
     if file not in {"OTA.py","main.py","config.py","boot.py","version.json"}:
