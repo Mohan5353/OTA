0342da637e144d8b641c809426be426f000c16e9
Mohan5353 <120332828+Mohan5353@users.noreply.github.com>
2023-11-13 23:23:21 +0530

    Update main.py

--- ESP32_CAM/main.py
+++ ESP32_CAM/main.py
@@ -2,21 +2,20 @@ import machine as mc
 import camera as cam
 from utime import sleep
 
-led=mc.Pin(2,mc.Pin.OUT)
+led = mc.Pin(2, mc.Pin.OUT)
 led.value(0)
 try:
     cam.init(0, format=cam.JPEG)
 except:
     pass
 else:
-    for mode in {"saturation"}
-        for i in {-2,-1,0,1,2}:
-            eval("cam.saturation(i)")
+    for mode in {"saturation", "contrast", "brightness"}:
+        for i in {-2, -1, 0, 1, 2}:
+            eval("cam.{}({})".format(mode, i))
             led.value(1)
-            img=cam.capture()
+            img = cam.capture()
             led.value(0)
-            file=open("/sd/images_esp/"+mode+'('+str(i)+')'+".jpeg","wb")
+            file = open("/sd/images_esp/" + mode + "(" + str(i) + ")" + ".jpeg", "wb")
             file.write(img)
             file.close()
             sleep(1.5)
-
