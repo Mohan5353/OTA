5a34fe43e1f2168edcb4b2bc6e0d6bd85433f1b9
Mohan5353 <120332828+Mohan5353@users.noreply.github.com>
2023-11-13 23:18:22 +0530

    Update main.py

--- ESP32_CAM/main.py
+++ ESP32_CAM/main.py
@@ -1,6 +1,22 @@
-# No implemented Code bro
-from uos import remove,listdir
-files=listdir()
-for file in files:
-    if file not in {"OTA.py","main.py","config.py","boot.py","version.json"}:
-        remove(file)
+import machine as mc
+import camera as cam
+from utime import sleep
+
+led=mc.Pin(2,mc.Pin.OUT)
+led.value(0)
+try:
+    cam.init(0, format=cam.JPEG)
+except:
+    pass
+else:
+    for mode in {"saturation"}
+        for i in {-2,-1,0,1,2}:
+            eval("cam.saturation(i)")
+            led.value(1)
+            img=cam.capture()
+            led.value(0)
+            file=open("/sd/images_esp/"+mode+'('+str(i)+')'+".jpeg","wb")
+            file.write(img)
+            file.close()
+            sleep(1.5)
+
